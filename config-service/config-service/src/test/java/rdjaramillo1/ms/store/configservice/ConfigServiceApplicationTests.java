f6125457d3feba0413419bb506934ba0c93a0056
renzodamian <renzojaramillocueva@gmail.com>
2022-01-30 18:59:47 -0500

    second commit

---
+++ config-service/config-service/src/test/java/rdjaramillo1/ms/store/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rdjaramillo1.ms.store.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
