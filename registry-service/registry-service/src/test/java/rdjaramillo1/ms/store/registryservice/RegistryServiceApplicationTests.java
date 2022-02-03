0c96de08c674adb113c326cd50da021c11efd787
renzodamian <renzojaramillocueva@gmail.com>
2022-02-02 20:23:47 -0500

    add config service-registry

---
+++ registry-service/registry-service/src/test/java/rdjaramillo1/ms/store/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rdjaramillo1.ms.store.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
